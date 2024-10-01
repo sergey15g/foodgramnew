4fe092ab10080e21f576e7f1afe6f9688c178092
Сергей Гордон <sergey15g@gmail.com>
2024-10-01 16:04:38 +0300

    Fix image decoding

--- backend/recipes/fields.py
+++ backend/recipes/fields.py
@@ -4,6 +4,13 @@ from django.core.files.base import ContentFile
 from rest_framework import serializers
 
 
+import base64
+import uuid
+from django.core.files.base import ContentFile
+
+from rest_framework import serializers
+
+
 class Base64ImageField(serializers.ImageField):
     def to_internal_value(self, data):
         # Если полученный объект строка, и эта строка
@@ -14,8 +21,11 @@ class Base64ImageField(serializers.ImageField):
             format_image, imgstr = data.split(";base64,")
             # И извлечь расширение файла.
             ext = format_image.split("/")[-1]
+            # Генерируем уникальное имя файла
+            file_name = f"{uuid.uuid4()}.{ext}"
             # Затем декодировать сами данные и поместить результат в файл,
-            # которому дать название по шаблону.
-            data = ContentFile(base64.b64decode(imgstr), name="temp." + ext)
+            # которому дать уникальное название.
+            data = ContentFile(base64.b64decode(imgstr), name=file_name)
 
         return super().to_internal_value(data)
+
